857369baf01a632c2186ef41fa3a80e03667ebd5
Kyou13 <kyou1013@gmail.com>
2017-12-02 02:52:38 +0900

    add 3 files

---
+++ getBoard.rb
@@ -0,0 +1,20 @@
+# 板情報取得
+
+# 外部ライブラリ読み込み
+require 'net/http'
+require 'uri'
+require 'json'
+
+def get_price
+  uri = URI.parse("https://api.bitflyer.jp") # パース
+  uri.path = '/v1/getboard' # 付加するparam
+  uri.query = ''
+  
+  # Net::HTTPオブジェクト生成
+  # params(address, port)
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true # SSL/TLS を利用するかどうか
+  response = https.get(uri.request_uri) # path + '?' + query」を文字列で返す
+  response_hash = JSON.parse(response.body)
+  puts response_hash["mid_price"]
+end
---
+++ test.rb
@@ -0,0 +1,4 @@
+require './getBoard.rb'
+require './buy'
+
+buy_order('SELL','400','0.01')
